d54078cdf91351bf78116034d277dd02da6421cb
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-12-17 23:41:02 -0500

    Completed project

--- python_3/projects/time_travel/custom_module.py
+++ python_3/projects/time_travel/custom_module.py
@@ -1,2 +1,9 @@
 def generate_time_travel_message(year, destination, cost):
     return f"If you would like to travel to {destination}, {year}: the cost for travel will be ${cost}."
+
+#------------------------------------------------------------------------------------------------
+
+# 🚀 "Time Traveler's Toolkit" project completed for Codecademy's "Learn Python 3" course:
+# https://www.codecademy.com/learn/learn-python-3
+
+#------------------------------------------------------------------------------------------------
--- python_3/projects/time_travel/time_travelers_toolkit.py
+++ python_3/projects/time_travel/time_travelers_toolkit.py
@@ -3,12 +3,31 @@ from decimal import Decimal
 from random import randint, choice # note use of comma here
 import custom_module
 
-date = dt.date.today()
-time = dt.datetime.now().time()
+destinations = ['Canada', 'Mexico', 'China', 'India', 'Germany', 'France', 'Russia', 'Korea', 'Japan', 'Indonesia', 'Egypt', 'Somalia', 'Greece', 'Turkey', 'Italy', 'Colombia', 'Brazil', 'Chile', 'Portugal', 'Uruguay', 'Spain', 'Iran', 'Ireland', 'Scotland', 'Nepal', 'Somalia', 'Namibia', 'Greenland', 'Iceland']
 
-print(date.strftime('%A, %B %d, %Y'))
-print(time.strftime('%H:%M %P')) # => pm
-print(time.strftime('%H:%M %p')) # => PM
-
+current_date = dt.date.today()
+current_time = dt.datetime.now().time()
+current_year = dt.date.today().year
+random_year = randint(1000, 3025)
 base_cost = Decimal('3000.00')
-print(base_cost)
+random_destination = choice(destinations)
+
+# Calculate cost multiplier based on the difference in years:
+cost_multiplier = abs(current_year - random_year)
+total_cost = (base_cost + Decimal("100") * Decimal(cost_multiplier)).quantize(Decimal('0.00'))
+
+print(f"The current date is: {current_date.strftime('%A, %B %d, %Y')}.")
+print(f"The current time is: {current_time.strftime('%H:%M %p')}.") # => '%p' results in 'AM', 'PM'; '%P' results im 'am', 'pm'
+
+message = custom_module.generate_time_travel_message(random_destination, random_year, total_cost) # Positional function arguments
+# Can also call with keyword arguments:
+# message = custom_module.generate_time_travel_message(year=random_year, destination=random_destination, cost=total_cost)
+
+print(message)
+
+#------------------------------------------------------------------------------------------------
+
+# 🚀 "Time Traveler's Toolkit" project completed for Codecademy's "Learn Python 3" course:
+# https://www.codecademy.com/learn/learn-python-3
+
+#------------------------------------------------------------------------------------------------
