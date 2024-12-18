ccbd2994deba8711a9bb3181b13665f5a5b93a2b
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-12-18 15:31:36 -0500

    Add Scrabble project

---
+++ python_3/projects/scrabble.py
@@ -0,0 +1,60 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+lowercase_letters = [letter.lower() for letter in letters]
+
+letter_to_points = {key:value for key, value in zip(letters, points)}
+# print(letter_to_points)
+
+letter_to_points[" "] = 0
+# print(letter_to_points)
+
+def score_word(word):
+  point_total = 0
+
+  for letter in word:
+    if letter in letter_to_points:
+      point_total += letter_to_points[letter]
+    else:
+      point_total += 0
+
+  return point_total
+
+brownie_points = score_word("BROWNIE")
+# print(brownie_points)
+
+player_to_words = {'player1': ['BLUE', 'TENNIS', 'EXIT'], 'wordNerd': ['EARTH', 'EYES', 'MACHINE'], 'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'], 'Prof Reader': ['ZAP', 'COMA', 'PERIOD']}
+
+player_to_points = {}
+
+def update_point_totals():
+  for player, words in player_to_words.items(): # initially I had written 'values' here
+    player_points = 0
+    # print(player)
+    # print(words)
+    for word in words:
+      player_points += score_word(word)
+    player_to_points[player] = player_points
+
+def play_word(player, word):
+  player_to_words[player].append(word)
+  update_point_totals()
+
+play_word('player1', 'HORSE')
+print(player_to_words)
+print(player_to_points)
+
+play_word('player1', 'BLUE')
+print(player_to_words)
+print(player_to_points)
+
+play_word('player1', 'APPLE')
+print(player_to_words)
+print(player_to_points)
+
+#------------------------------------------------------------------------------------------------
+
+# *️⃣ Scrabble project completed for Codecademy's "Learn Python 3" course:
+# https://www.codecademy.com/learn/learn-python-3
+
+#------------------------------------------------------------------------------------------------
