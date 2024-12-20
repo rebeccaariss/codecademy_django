f81086ba83e4b1a7252d3dac5b03a97e81f78e3a
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-12-19 23:27:49 -0500

    Clean up code and add annotations

--- python_3/projects/scrabble.py
+++ python_3/projects/scrabble.py
@@ -1,14 +1,22 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+# ✅ Given the provided table, create a dictionary that maps players to the list of words they've played:
+player_to_words = {'player1': ['BLUE', 'TENNIS', 'EXIT'], 'wordNerd': ['EARTH', 'EYES', 'MACHINE'], 'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'], 'Prof Reader': ['ZAP', 'COMA', 'PERIOD']}
+
+# ✅ Initialize empty dictionary for scoring points:
+player_to_points = {}
+
+# ✅ TODO: Make the letter_to_points dictionary able to handle lowercase inputs as well:
 lowercase_letters = [letter.lower() for letter in letters]
 
+# ✅ Combine the provided 'letters' and 'points' lists into a dictionary with letters mapped to their respective point values:
 letter_to_points = {key:value for key, value in zip(letters, points)}
-# print(letter_to_points)
 
+# ✅ Update 'letter_to_points' dictionary to account for blank tiles:
 letter_to_points[" "] = 0
-# print(letter_to_points)
 
+# ✅ Looping through a given word, return its point value:
 def score_word(word):
   point_total = 0
 
@@ -20,36 +28,35 @@ def score_word(word):
 
   return point_total
 
-brownie_points = score_word("BROWNIE")
+# ✅ Test function:
+# brownie_points = score_word("BROWNIE")
 # print(brownie_points)
 
-player_to_words = {'player1': ['BLUE', 'TENNIS', 'EXIT'], 'wordNerd': ['EARTH', 'EYES', 'MACHINE'], 'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'], 'Prof Reader': ['ZAP', 'COMA', 'PERIOD']}
-
-player_to_points = {}
-
+# ✅ Create a function to update all player point totals when called, looping over 1) players/word lists ('.items') and 2) over each word in the words list:
 def update_point_totals():
-  for player, words in player_to_words.items(): # initially I had written 'values' here
+  # '.items', not '.values', returns tuples with key/value pairs:
+  for player, words in player_to_words.items():
     player_points = 0
-    # print(player)
-    # print(words)
     for word in words:
       player_points += score_word(word)
     player_to_points[player] = player_points
 
+# ✅ Create a function that represents a "play", appending the word to the player's word list and calling the update_point_totals function for the latest score:
 def play_word(player, word):
   player_to_words[player].append(word)
   update_point_totals()
 
+# ✅ Test for plays & points (including lowercase letters):
 play_word('player1', 'HORSE')
-print(player_to_words)
+print(f'\n{player_to_words}')
 print(player_to_points)
 
 play_word('player1', 'BLUE')
-print(player_to_words)
+print(f'\n{player_to_words}')
 print(player_to_points)
 
-play_word('player1', 'APPLE')
-print(player_to_words)
+play_word('player1', 'apple')
+print(f'\n{player_to_words}')
 print(player_to_points)
 
 #------------------------------------------------------------------------------------------------
